eb30e3b335b9e96a409695f3343c2ac4b178f8e7
Kevin dsa <kevin0108dsa@gmail.com>
2023-06-06 12:34:53 -0400

    Update 01notauthworkingpage.cy.js

--- client/cypress/e2e/01notauthworkingpage.cy.js
+++ client/cypress/e2e/01notauthworkingpage.cy.js
@@ -2,9 +2,7 @@ describe('template spec', () => {
   it('visit error page', () => {
     cy.visit('/error')
   })
-  it('visit unauthorised dashboard', () => {
-    cy.visit('/customerdashboard')
-  })
+ 
   
 })
 
